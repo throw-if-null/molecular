113c4a63af38ae9054154d100b825115d1615a78
Mirza Merdovic <mirza@climateview.global>
2026-01-12 12:35:21 +0100

    tests(silicon): add hook execution tests and helpers

--- internal/silicon/hooks_test.go
+++ internal/silicon/hooks_test.go
@@ -2,7 +2,6 @@ package silicon_test
 
 import (
 	"context"
-	"database/sql"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -10,8 +9,8 @@ import (
 	"time"
 
 	"github.com/throw-if-null/molecular/internal/api"
+	"github.com/throw-if-null/molecular/internal/lithium"
 	"github.com/throw-if-null/molecular/internal/silicon"
-	"github.com/throw-if-null/molecular/internal/store"
 	_ "modernc.org/sqlite"
 )
 
